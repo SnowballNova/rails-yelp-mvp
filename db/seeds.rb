f15eca982e0d7544ee176185156d3580fa5a2139
Charles Boucher <charlesb.ipag@gmail.com>
2024-11-13 14:42:43 +0100

    restaurants & reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Creating restaurants'
+Restaurant.create(name: "SuperBao",
+                  address: 'Strasbourg Saint Denis',
+                  category: 'japanese',
+                  phone_number: '0123456789');
+
+Restaurant.create(name: "Pino",
+                  address: 'Réaumur',
+                  category: 'italien',
+                  phone_number: '0177896789');
+
+Restaurant.create(name: "Matsuri",
+                  address: 'Bourse',
+                  category: 'japanese',
+                  phone_number: '0199756789');
+
+Restaurant.create(name: "Trois fois plus de piment",
+                  address: 'Bonne nouvelle',
+                  category: 'chinese',
+                  phone_number: '0144756789');
+
+Restaurant.create(name: "Barbac",
+                  address: 'Sentier',
+                  category: 'french',
+                  phone_number: '0111756789');
+
+puts "'#{Restaurant.count}' restaurant created"
